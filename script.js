2baf116a5d92cdab160b0ee2eeec280fd3bbd08a
Aby George <abygeorge133@gmail.com>
2023-12-08 15:50:42 +0530

    Add basic functions

---
+++ script.js
@@ -0,0 +1,37 @@
+let firstNumber
+let secondNumber
+let operator
+
+
+
+
+
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function operate(operator,num1,num2){
+    if(operator == '+'){
+        console.log(add(num1,num2))
+    } else if(operator == '-'){
+        console.log(subtract(num1,num2))
+    } else if(operator == '*'){
+        console.log(multiply(num1,num2))
+    } else if(operator == '/'){
+       console.log(divide(num1,num2))
+    }
+
+}
+
