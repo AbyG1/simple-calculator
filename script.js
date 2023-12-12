386d04416fecfe7a69ed8016ccfc6fa18f81c539
Aby George <abygeorge133@gmail.com>
2023-12-12 16:44:22 +0530

    Update design

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ let secondNumber
 let operator
 
 
+let displayLargeEl = document.getElementById('display-low')
+let displaySmallEl = document.getElementById('display-up')
 
+let buttons = document.querySelectorAll('button')
 
+buttons.forEach((button) => {
+    button.addEventListener('click',() => {
+    let value = button.textContent
+    displayLargeEl.textContent += value
+    })
+})
+
+    
 
 function add(a,b){
     return a+b
